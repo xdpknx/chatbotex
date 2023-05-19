cdb958d3264d30e3f443f786a1e75e9eb957ef77
Deepak Narayan <dnarayan@emdesic.dk>
2023-05-19 20:34:32 +0200

    FIX: minor errors

--- query_data.py
+++ query_data.py
@@ -1,7 +1,8 @@
 """Create a ChatVectorDBChain for question/answering."""
-from langchain.callbacks.base import AsyncCallbackManager
+from langchain.callbacks.manager import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
 from langchain.chains import ChatVectorDBChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT,
                                                      QA_PROMPT)
 from langchain.chains.llm import LLMChain
@@ -12,7 +13,7 @@ from langchain.vectorstores.base import VectorStore
 
 def get_chain(
     vectorstore: VectorStore, question_handler, stream_handler, tracing: bool = False
-) -> ChatVectorDBChain:
+) -> ConversationalRetrievalChain:  #  <== CHANGE THE TYPE
     """Create a ChatVectorDBChain for question/answering."""
     # Construct a ChatVectorDBChain with a streaming llm for combine docs
     # and a separate, non-streaming llm for question generation
@@ -45,8 +46,9 @@ def get_chain(
         streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
     )
 
-    qa = ChatVectorDBChain(
-        vectorstore=vectorstore,
+    qa = ConversationalRetrievalChain(         # <==CHANGE  ConversationalRetrievalChain instead of ChatVectorDBChain
+        # vectorstore=vectorstore,             # <== REMOVE THIS
+        retriever=vectorstore.as_retriever(),  # <== ADD THIS
         combine_docs_chain=doc_chain,
         question_generator=question_generator,
         callback_manager=manager,
